844621f9ee72ae0c12a51aaab4b1d23c6d9bdbb7
Soldeplata Saketos Candela <soldeplatadeveloper@gmail.com>
2020-02-04 17:22:06 +0100

    test removeAppToHostsPlugin

--- src/scripts/Apdex/removeAppToHostsPlugin.js
+++ src/scripts/Apdex/removeAppToHostsPlugin.js
@@ -7,20 +7,18 @@
  * @param {function} hostAppEntryGarbager removes corresponding App (ordered by apdex) to each Host of rankedAppsByHost Map
  */
 export const removeAppToHostsPlugin = (list, rankedAppsByHost, hostAppEntryGarbager) => {
+  if (!(list && rankedAppsByHost && hostAppEntryGarbager)) {
+    throw new Error('missing parameters');
+  }
   /**
    * Calls hostAppEntryGarbager to remove from the ranked array of AppData in each of the appData.host entries the `appData` received
    * @param {AppData} appData
    */
   const removeAppToHosts = appData => {
-    try {
-      hostAppEntryGarbager(rankedAppsByHost)(appData);
-      const index = list.indexOf(appData);
-      list.splice(index, 1); // this line represents persisting the data into the source
-      return appData; // returning the deleted element just in case it is needed later to be restored
-    } catch (error) {
-      console.error(error); // TODO: error manager (Sentry?)
-      return false;
-    }
+    hostAppEntryGarbager(rankedAppsByHost)(appData);
+    const index = list.indexOf(appData);
+    list.splice(index, 1); // this line represents persisting the data into the source
+    return appData; // returning the deleted element just in case it is needed later to be restored
   };
   return Object.freeze({
     removeAppToHosts,
