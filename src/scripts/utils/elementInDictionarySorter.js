c64a3b2bb4c841c514842a0282682687a0684548
Soldeplata Saketos Candela <soldeplatadeveloper@gmail.com>
2020-02-04 07:41:22 +0100

    test elementInDictionarySorter

--- src/scripts/utils/elementInDictionarySorter.js
+++ src/scripts/utils/elementInDictionarySorter.js
@@ -1,18 +1,22 @@
 import { insertByOrder } from '@/scripts/utils';
-// TODO:  tests
 /**
  * Inserts in the value (an array) of a dictionary an `element` ordered by the `sortBy` function
  * @param {Map<*,[]>} dictionary
  * @param {*} element
  * @param {(a:*,b:*) => boolean} sortBy
  */
-export const elementInDictionarySorter = (dictionary, element, sortBy) => key => {
-  if (dictionary.has(key)) {
-    // working with reference types we don't need to reset the array
-    // but simply modify it in place
-    const arrayOfEntries = dictionary.get(key);
-    insertByOrder(arrayOfEntries, element, sortBy);
-  } else {
-    dictionary.set(key, [element]);
+export const elementInDictionarySorter = (dictionary, element, sortBy) => {
+  if (!(dictionary && element && sortBy)) {
+    throw new Error('missing arguments');
   }
+  return key => {
+    if (dictionary.has(key)) {
+      // working with reference types we don't need to reset the array
+      // but simply modify it in place
+      const arrayOfEntries = dictionary.get(key);
+      insertByOrder(arrayOfEntries, element, sortBy);
+    } else {
+      dictionary.set(key, [element]);
+    }
+  };
 };
---
+++ src/scripts/utils/elementInDictionarySorter.spec.js
@@ -0,0 +1,43 @@
+import { elementInDictionarySorter } from './elementInDictionarySorter';
+import { insertByOrder } from '@/scripts/utils';
+
+jest.mock('@/scripts/utils');
+describe('elementInDictionarySorter', () => {
+  const key = 'foo';
+  const sortBy = (a, b) => a > b;
+  const arrayOfEntries = [];
+  let dictionary;
+  beforeEach(() => {
+    jest.resetAllMocks();
+    dictionary = new Map([[key, arrayOfEntries]]);
+  });
+  describe('Given any missing parameter', () => {
+    it('should throw', () => {
+      expect(() => elementInDictionarySorter(null, 42, sortBy)).toThrow();
+      expect(() => elementInDictionarySorter(dictionary, null, sortBy)).toThrow();
+      expect(() => elementInDictionarySorter(dictionary, 42, null)).toThrow();
+      expect(() => elementInDictionarySorter(null, null, sortBy)).toThrow();
+      expect(() => elementInDictionarySorter(dictionary, null, null)).toThrow();
+      expect(() => elementInDictionarySorter(null, 42, null)).toThrow();
+      expect(() => elementInDictionarySorter()).toThrow();
+    });
+  });
+  describe('Given all required parameters', () => {
+    describe('Given an initilized key ', () => {
+      const element = 3;
+      it('should call insertByOrder', () => {
+        elementInDictionarySorter(dictionary, element, sortBy)(key);
+        expect(insertByOrder).toBeCalledTimes(1);
+        expect(insertByOrder).toBeCalledWith(arrayOfEntries, element, sortBy);
+      });
+    });
+    describe('Given a NOT initilized key ', () => {
+      const element = { foo: 'bar' };
+      it('should set an array with the element', () => {
+        const notInitializedKey = 'notInitializedKey';
+        elementInDictionarySorter(dictionary, element, sortBy)(notInitializedKey);
+        expect(dictionary.get(notInitializedKey)).toMatchObject([element]);
+      });
+    });
+  });
+});
