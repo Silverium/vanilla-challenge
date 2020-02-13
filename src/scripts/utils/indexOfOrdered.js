d15d1343aadeb3c85867ee5d0037757695646b07
Soldeplata Saketos Candela <soldeplatadeveloper@gmail.com>
2020-02-13 17:32:22 +0100

    getSameValues first approach

--- src/scripts/utils/indexOfOrdered.js
+++ src/scripts/utils/indexOfOrdered.js
@@ -1,13 +1,64 @@
-/**
- * Given an ordered collection of primitive values following the OrderBy condition,
- * it returns the index of the element
- * @param {array} collection
- * @param {*} element
- * @param {{getValue:(element:*)=>string|number, isDescending: boolean}} options
- * @param options.getValue it should return the value used to order the collection
- * @param options.isDescending
- */
-export const indexOfOrdered = (collection = [], element, options) => {
+export const getSameValues = (collection, element, options = {}) => {
+  const { length } = collection;
+  const { getValue = value => value, isLastElement = false } = options;
+  const minBoundary = 0;
+  const maxBoundary = length - 1;
+
+  const maxLoopsToResolve = Math.floor(Math.log2(length));
+  const value = getValue(element);
+  let index = Math.floor(length / 2);
+  let loops = 0;
+  let min = minBoundary;
+  let max = maxBoundary;
+  // check first element to the right with a value higher than the given one
+  if (!isLastElement) {
+    while (loops <= maxLoopsToResolve && index <= maxBoundary && index >= minBoundary) {
+      if (getValue(collection[index]) === value) {
+        // move to the right
+        min = index;
+        index += Math.ceil((max - min) / 2);
+        if (max === index) {
+          return min;
+        }
+      } else {
+        // move to the left
+        max = index;
+        index -= Math.ceil((max - min) / 2);
+        if (min === index) {
+          return min;
+        }
+      }
+      loops += 1;
+    }
+  }
+  // isLastElement
+  while (loops <= maxLoopsToResolve && index <= maxBoundary && index >= minBoundary) {
+    if (getValue(collection[index]) === value) {
+      // move to the left
+      max = index;
+      index -= Math.ceil((max - min) / 2);
+      if (min === index) {
+        return max;
+      }
+    } else {
+      // move to the right
+      min = index;
+      index += Math.ceil((max - min) / 2);
+      if (max === index) {
+        return max;
+      }
+    }
+    loops += 1;
+  }
+  return -1;
+};
+console.log(
+  getSameValues([13, 13, 13, 13, 13, 13, 13, 13, 13, 0, 1, 2, 3, 4], 13, {
+    isLastElement: false,
+    isDescending: true,
+  })
+);
+export const indexOfSimpleOrdered = (collection = [], element, options = {}) => {
   if (element !== 0 && !element) {
     throw new Error('missing parameters');
   }
@@ -31,8 +82,12 @@ export const indexOfOrdered = (collection = [], element, options) => {
   let step;
   let min = minBoundary;
   let max = maxBoundary;
-  while (loops <= maxLoopsToResolve && index < maxBoundary && index >= minBoundary) {
+  while (loops <= maxLoopsToResolve && index <= maxBoundary && index >= minBoundary) {
     if (getValue(collection[index]) === getValue(element)) {
+      // find how many elements to the left exist with the same value and set min
+      // find how many elements to the right and set max
+      // 1.1 no secondary condition => get the first one (last for descending)
+      // 1.2 call this function withsecondary condition
       break;
     }
     const isToTheEnd = isDescending
@@ -53,4 +108,33 @@ export const indexOfOrdered = (collection = [], element, options) => {
     loops += 1;
   }
   return index;
+};
+
+/**
+ * Given an ordered collection of primitive values following the OrderBy condition,
+ * it returns the index of the element
+ * @param {array} collection
+ * @param {*} element
+ * @param {{getValue:(element:*)=>string|number, isDescending: boolean}} options
+ * @param options.getValue it should return the value used to order the collection
+ * @param options.isDescending
+ */
+export const indexOfOrdered = (collection = [], element, options = {}) => {
+  if (element !== 0 && !element) {
+    throw new Error('missing parameters');
+  }
+  const { length } = collection;
+  if (!length) {
+    return -1;
+  }
+  const optionsList = [].concat(options);
+  let optionsIndex = 0;
+  let result;
+  while (!result && optionsIndex < optionsList.length) {
+    if (optionsList.length === 1) {
+      return indexOfSimpleOrdered(collection, element, optionsList[0]);
+    }
+    optionsIndex += 1;
+  }
+  return -1;
 };
--- src/scripts/utils/indexOfOrdered.spec.js
+++ src/scripts/utils/indexOfOrdered.spec.js
@@ -1,4 +1,4 @@
-import { indexOfOrdered } from './indexOfOrdered';
+import { indexOfOrdered, getSameValues } from './indexOfOrdered';
 
 const minSize = 5000;
 const maxSize = 5001;
@@ -8,6 +8,7 @@ const objects = size =>
   Array.from(Array(size), (element, index) => ({
     value: size - index,
   }));
+const valueTimes = (value, times) => Array.from(Array(times), () => value);
 describe('indexOfOrdered', () => {
   describe('Given values as numbers', () => {
     for (let size = minSize; size < maxSize; size += 1) {
@@ -20,7 +21,7 @@ describe('indexOfOrdered', () => {
       });
     }
   });
-  describe('Given vales as objects', () => {
+  describe('Given values as objects', () => {
     const cases = [
       [137, objects(2000)],
       [1, objects(2001)],
@@ -37,6 +38,47 @@ describe('indexOfOrdered', () => {
       expect(result).toBe(expected);
     });
   });
+  describe.skip('Given collections with repeated values', () => {
+    describe('When no secondary criteria is given', () => {
+      it('should return the first object with the same value', () => {
+        const arr = [0, 1, 1, 2, 2, 2, 2, 2, 4, 5, 6, 7, 8, 9, 10];
+        const value = 2;
+        const expected = 3;
+        const result = indexOfOrdered(arr, value);
+        expect(result).toBe(expected);
+      });
+    });
+  });
   // TODO: ascending collections
   // TODO: non linear collections ([1,5,6,23,45...])
+});
+describe('getSameValues', () => {
+  it('should return start and end of indexes of same values', () => {
+    const result = getSameValues([2, 2, 2, 2, 2, 2, 6], 2);
+    expect(result).toBe(5);
+  });
+  it('should return end and start of indexes of same values', () => {
+    const result = getSameValues([2, 2, 2, 2, 4, 5, 6, 6, 6, 6, 6], 6, { isLastElement: true });
+    expect(result).toBe(6);
+  });
+  const table = [
+    [true, 5, 9, [0, 1, 2, 3, 4, ...valueTimes(9, 2)]],
+    [true, 5, 11, [0, 1, 2, 3, 4, ...valueTimes(11, 7)]],
+    [true, 5, 13, [0, 1, 2, 3, 4, ...valueTimes(13, 13)]],
+    [true, 5, 21, [0, 1, 2, 3, 4, ...valueTimes(21, 21)]],
+    [true, 5, 101, [0, 1, 2, 3, 4, ...valueTimes(101, 101)]],
+    [false, 1, 9, [...valueTimes(9, 2), ...valueTimes(9000, 12)]],
+    [false, 6, 11, [...valueTimes(11, 7), ...valueTimes(9000, 12)]],
+    [false, 12, 13, [...valueTimes(13, 13), ...valueTimes(9000, 12)]],
+    [false, 20, 21, [...valueTimes(21, 21), ...valueTimes(9000, 12)]],
+    [false, 100, 101, [...valueTimes(101, 101), ...valueTimes(9000, 12)]],
+    [false, 100, 101, [...valueTimes(101, 101), ...valueTimes(9000, 1200)]],
+  ];
+
+  it.each(table)(
+    'a %s last element will have %s repeated from/to index %s',
+    (isLastElement, expected, element, collection) => {
+      expect(getSameValues(collection, element, { isLastElement })).toBe(expected);
+    }
+  );
 });
