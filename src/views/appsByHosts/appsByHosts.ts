20f7d4995f53ff37bd935e574de248d9cd949eb9
Soldeplata Saketos Candela <soldeplatadeveloper@gmail.com>
2020-02-20 08:24:20 +0100

    improved performance of listeners

--- src/views/appsByHosts/appsByHosts.ts
+++ src/views/appsByHosts/appsByHosts.ts
@@ -16,7 +16,7 @@ export const appsByHosts = {
     const { childNodes: labelNodes } = checkboxNode.parentElement;
     const labelTextNode = labelNodes[labelNodes.length - 1];
     const classesToToggle = ['grid-mode', 'd-flex', 'flex-wrap'];
-    checkboxNode.addEventListener('click', (event : MouseEvent ) => {
+    checkboxNode.addEventListener('click', (event: MouseEvent) => {
       const isCheckbox = event.target as HTMLInputElement;
       if (isCheckbox.checked) {
         isGridMode = true;
@@ -47,43 +47,45 @@ export const appsByHosts = {
         modalManager.show();
       });
     });
+    // adding one event listener for each hosts-list node. It feels more sensitive than one for all the view
+    const hostsLists = document.getElementsByClassName('hosts-list');
+    Array.prototype.forEach.call(hostsLists, (hostListNode: HTMLElement) => {
+      hostsListNode.classList.add('cursor-pointer');
+      hostListNode.addEventListener('click', (event: MouseEvent) => {
+        const target = event.target;
+        const appInHost: HTMLElement = (target as HTMLElement).closest('.appInHost');
+        if (appInHost) {
+          event.stopPropagation();
+          const { appName, rankingIndex, hostName } = appInHost.dataset;
+          modalManager.setHeader(appName);
+          const hostJson = window.Apdex.getTopAppsByHost(hostName)[rankingIndex];
+          const deleteButtonId = hostJson.name.replace(/[\W]\d/, '');
+          const buttonHtml = templateTag({
+            tag: 'button',
+            content: 'Delete',
+            attributes: [
+              {
+                key: 'id',
+                value: deleteButtonId,
+              },
+              { key: 'class', value: 'btn-danger' },
+            ],
+          });
 
-    const appInHosts = document.getElementsByClassName('appInHost');
-
-    for (let hostIndex = 0; hostIndex < appInHosts.length; hostIndex += 1) {
-      const hostNode = appInHosts[hostIndex] as HTMLElement;
-      hostNode.classList.add('cursor-pointer');
-      hostNode.addEventListener('click', () => {
-        const { appName, rankingIndex, hostName } = hostNode.dataset;
-        modalManager.setHeader(appName);
-        const hostJson = window.Apdex.getTopAppsByHost(hostName)[rankingIndex];
-        const deleteButtonId = hostJson.name.replace(/[\W]\d/, '');
-        const buttonHtml = templateTag({
-          tag: 'button',
-          content: 'Delete',
-          attributes: [
-            {
-              key: 'id',
-              value: deleteButtonId,
-            },
-            { key: 'class', value: 'btn-danger' },
-          ],
-        });
-
-        const versionHtml = templateTag({ content: `version: ${hostJson.version}` });
-        const wrapper = templateTag({
-          attributes: [{ key: 'class', value: 'd-flex justify-content-between' }],
-          content: versionHtml + buttonHtml,
-        });
-        modalManager.setBody(wrapper);
-        document.getElementById(deleteButtonId).onclick = () => {
-          window.Apdex.removeAppToHosts(hostJson);
-          const currentcheckboxNode = document.getElementById('listModeCheckbox') as HTMLInputElement;
-
-          this.load({ isGridMode: currentcheckboxNode.checked });
-        };
-        modalManager.show();
-      });
-    }
+          const versionHtml = templateTag({ content: `version: ${hostJson.version}` });
+          const wrapper = templateTag({
+            attributes: [{ key: 'class', value: 'd-flex justify-content-between' }],
+            content: versionHtml + buttonHtml,
+          });
+          modalManager.setBody(wrapper);
+          document.getElementById(deleteButtonId).onclick = () => {
+            window.Apdex.removeAppToHosts(hostJson);
+            const currentcheckboxNode = document.getElementById('listModeCheckbox') as HTMLInputElement;
+            this.load({ isGridMode: currentcheckboxNode.checked });
+          };
+          modalManager.show();
+        }
+      })
+    })
   },
 };
